f2469b5b7e6b845248a970dfeb9b611afea58383
ItsSheldonDev <github@sheldon-dev.fr>
2024-12-22 02:06:29 +0100

    Adding ORM Services 📂

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,31 @@
+// src/prisma/prisma.service.ts
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  constructor() {
+    super({
+      log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
+    });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+
+  async cleanDatabase() {
+    if (process.env.NODE_ENV === 'development') {
+      return Promise.all([
+        this.file.deleteMany(),
+        this.user.deleteMany(),
+        this.settings.deleteMany(),
+        this.statistics.deleteMany(),
+      ]);
+    }
+  }
+}
